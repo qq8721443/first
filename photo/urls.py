fd0a4abcefece2dd29923d1971c044bf8ead8437
qq8721443 <qq8721443@naver.com>
2020-04-04 01:15:28 +0900

    error fix

--- photo/urls.py
+++ photo/urls.py
@@ -4,6 +4,6 @@ from .views import PhotoLV, PhotoDV, PhotologLV, PhotologDV
 app_name='photo'
 urlpatterns = [
     path('', PhotologLV.as_view(), name='photolog_list'),
-    path('/list/<int:pk>/', PhotologDV.as_view(), name='photolog_detail'),
+    path('list/<int:pk>/', PhotologDV.as_view(), name='photolog_detail'),
 
 ]
